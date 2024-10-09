f827c1ce6b2dd7bdacc2fa611192c711ef658a3b
Harshit Shrivastava <harshit10a@gmail.com>
2024-10-09 16:19:09 +0530

    First Commit

---
+++ TREE/src/INorder_Traversal.java
@@ -0,0 +1,30 @@
+public class INorder_Traversal {
+    public static class Node {
+        int val;
+        Node left;
+        Node rigth;
+        public Node(int val){
+            this.val=val;
+        }
+    }
+    public static void inorder(Node root){
+        if (root==null)return;
+        inorder(root.left);
+        System.out.println(root.val);
+        inorder(root.rigth);
+    }
+    public static void main(String[] args) {
+        Node root = new Node(100);
+        Node a = new Node(20);
+        Node b = new Node(30);
+        Node c = new Node(40);
+        Node d = new Node(50);
+        Node e = new Node(60);
+        Node f = new Node(70);
+        root.left=a;root.rigth=b;
+        a.left=c;a.rigth=d;
+        b.left=e;b.rigth=f;
+        inorder(root);
+
+    }
+}
